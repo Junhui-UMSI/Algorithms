7cfe8df53afc2086667e327c32c6a27e3aa27543
Junhui Li <junhui@umich.edu>
2016-10-12 15:43:02 -0400

    double linkedlist

--- SingleLinkedList.js
+++ SingleLinkedList.js
@@ -8,22 +8,26 @@ sll = new SingleList();
 console.log(sll);
 
 SingleList.prototype.push = function(value) {
-  var node = {
-    value: value,
-    next: null
-  }
-  if(this.head === null){
-     this.head = node;
-  }
-  else{
-     currentNode = this.head;
-     while(currentNode.next){
-        currentNode = currentNode.next;
-     }
-     currentNode.next = node;
-  }
-  this._length++;
+var node = {
+  value: value,
+  next: null,
+  prev: null
 }
+if(this.head === null){
+  this.head = node
+}
+else{
+  currentnode = this.head;
+  while(currentnode.next){
+    currentnode = currentnode.next;
+  }
+  currentnode.next = node;
+  node.prev = currentnode;
+}
+this._length++;
+}
+
+
 sll.push(2);
 sll.push(3);
 sll.push(4);
