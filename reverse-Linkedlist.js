84d1b6c852a272d3febdf972ece37fdb449d2d57
Junhui Li <junhui@umich.edu>
2016-10-24 23:33:47 -0400

    Create reverse-Linkedlist.js

---
+++ reverse-Linkedlist.js
@@ -0,0 +1,10 @@
+var reverseList = function(head) {
+    var current = head;
+    var prev = null;
+    while(current){
+        var next = current.next; // get the next element
+        current.next = prev; // change the pointer of the current element
+        prev = current;  // reset prev element
+        current = next; //reset current element, start the recurison
+    }
+};
